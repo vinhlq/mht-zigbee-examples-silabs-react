207e3aa45cad5fc79468ec02e0f6c584a3111ee2
Vinh Le <vinhlq@hotmail.com>
2020-03-15 22:34:22 +0700

    ...

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # NODE=/home/deb/.nvm/versions/node/v8.10.0/bin/node
 NODE_BIN=${NODE}
-PWD=$(readlink -e $(dirname $0))
+PWD=$(readlink -f $(dirname $0))
 export env $(cat ${PWD}/.env | grep -v '^#' | xargs)
 [ -z "${NODE_BIN}" ] && NODE_BIN=${NODE}
 [ -z "${NODE_BIN}" ] && (>&2 echo "set 'NODE' environment variable") && exit 1
